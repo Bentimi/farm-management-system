4c06fb95cb04e0359b4f25c117710886eb668c6c
Ogunkunle Benjamin Timileyin <110978324+Bentimi@users.noreply.github.com>
2026-04-17 17:23:41 +0100

    Update payment.service.js

--- services/payment.service.js
+++ services/payment.service.js
@@ -138,10 +138,12 @@ const createRedirectUrl = async (userId, data) => {
             })
         } else {
             createOrder = await tx.order.create({
+                data: {
                 status: 'pending',
                 orderedUserId: userAuth.id,
                 txRef: txRef,
                 total_price: totalAmount
+               }
             })
         }
 
